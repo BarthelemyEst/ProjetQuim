38f7ad276cf07e03af2df72816156e9ed3ea34a1
BarthelemyEst <74047314+BarthelemyEst@users.noreply.github.com>
2024-04-19 18:29:31 +0200

    Initial Commit

---
+++ src/main/java/com/quim/QuimApplication.java
@@ -0,0 +1,13 @@
+package com.quim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quim/QuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
